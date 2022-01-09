c470243b8fbc8b2ec77b824abb6fa19899ac9cd8
sopank <sopank@Brahma.hitronhub.home>
2022-01-09 02:51:58 -0600

    Added Destination module with redirect

--- destination/urls.py
+++ destination/urls.py
@@ -3,5 +3,7 @@ from . import views
 from django.contrib import admin
 
 urlpatterns = [
-    path('new_dest', views.new_dest, name='new_dest')
+#    path('new_dest', views.new_dest, name='new_dest')
+    path('new_dest/<str:city_name>', views.new_dest, name='new_dest')
+   
 ]
